b5eca1c4db1f4a526e16a61a0fbcc53a7143ec93
semiteprofessor <semiteprofessor@gmail.com>
2021-11-04 19:54:11 +0100

    StoreKeeper

---
+++ src/controller/Main.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+import animatefx.animation.*;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.effect.Glow;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+/**
+ *
+ * @author GeoCodec
+ */
+public class Main extends Application {
+
+    /**
+     * @param stage
+     * @throws Exception
+     */
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/schoolmanagementsystem/View/Application.fxml"));
+
+        Scene scene = new Scene(root);
+        scene.setFill(Color.TRANSPARENT);
+
+        stage.setScene(scene);
+        stage.initStyle(StageStyle.TRANSPARENT);
+
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
